5e5002f11757253efaadd8ce2f254609b7ac7f5a
Alfredchong726 <alfredchong260@gmail.com>
2024-04-25 20:29:42 +0800

    add lecturer and presentation classes

---
+++ src/main/java/com/example/Presentation.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.Date;
+
+public class Presentation {
+    public String presentationId;
+    public String reportId;
+    public Date dateOfPresentation;
+    public String slot;
+}
