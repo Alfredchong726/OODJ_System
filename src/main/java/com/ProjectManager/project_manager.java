82ba676245eea8702313171b7c77c761489ebffc
fabiantang19 <fabiantang@outlook.my>
2024-05-07 16:47:56 +0800

    Admin class with Function

---
+++ src/main/java/com/ProjectManager/project_manager.java
@@ -0,0 +1,42 @@
+package com.example;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author fabia
+ */
+public class project_manager {
+    private String pmid;
+    private String name;
+    private String password;
+    
+    public project_manager(String pmid, String name, String passwor){
+        this.pmid = pmid;
+        this.name = name;
+        this.password = password;
+    }
+    
+    public void allotStudentToAssessment(){
+        
+        System.out.println("");
+    }
+    
+    public void assignSupervisorjs(){
+        
+        System.out.println("");
+    }
+    
+    public void asignSupervisors(){
+        
+        System.out.println("");
+    }
+    
+    public void viewReportStatus(){
+        
+        System.out.println("");
+    }
+}
