dda94b348a9f6b107a4405b027c2dd260ffc53a5
fabiantang19 <fabiantang@outlook.my>
2024-05-07 16:59:40 +0800

    Renname package name from package com.example to package com.Admin and Project Manager

--- src/main/java/com/Admin/admin.java
+++ src/main/java/com/Admin/admin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.Admin;
 
 
 import java.io.FileWriter;
--- src/main/java/com/ProjectManager/project_manager.java
+++ src/main/java/com/ProjectManager/project_manager.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.ProjectManager;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
